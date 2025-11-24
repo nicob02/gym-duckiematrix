406abaaab0ca68ff2c0bc025c95d635b8d3b4f67
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-11-24 13:42:04 -0500

    Create random_policy.py

---
+++ src/random_policy.py
@@ -0,0 +1,18 @@
+from gym_duckiematrix.DB21J import DuckiematrixDB21JEnv
+import time
+
+env = DuckiematrixDB21JEnv(entity_name="map_0/vehicle_0")
+obs, info = env.reset()
+
+try:
+    for step in range(500):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            obs, info = env.reset()
+        time.sleep(0.1)  # 10 Hz control rate
+finally:
+    # Cleanup
+    env.robot.camera.stop()
+    env.robot.motors.stop()
+    env.robot.reset_flag.stop()
